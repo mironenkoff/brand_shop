eed40ca0dac11b4a2a0bab1cde654d14272d925c
mironenkoff <mironenkoff@mail.ru>
2018-08-16 08:11:33 +0300

    Пилю подключение к БД

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,18 @@
 <?php
+//Подключение к БД www.000webhost.com:
+//$dsn = 'mysql:dbname=id3467371_brand;host=localhost';
+//$user = 'id3467371_brandadmin';
+//$password = 'branDadmin007';
+
+//Подключение к БД openserver:
+$dsn = 'mysql:dbname=brand;host=localhost';
+$user = 'root';
+$password = '';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Подключение не удалось: ' . $e->getMessage();
+}
 
 include 'view.php';
