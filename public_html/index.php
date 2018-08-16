a3e1de646104f0582b73bf4e87cae1d858911956
mironenkoff <mironenkoff@mail.ru>
2018-08-16 08:21:21 +0300

    Пилю подключение к БД

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,12 @@
 <?php
 //Подключение к БД www.000webhost.com:
+
 //$dsn = 'mysql:dbname=id3467371_brand;host=localhost';
 //$user = 'id3467371_brandadmin';
 //$password = 'branDadmin007';
 
 //Подключение к БД openserver:
+
 $dsn = 'mysql:dbname=brand;host=localhost';
 $user = 'root';
 $password = '';
